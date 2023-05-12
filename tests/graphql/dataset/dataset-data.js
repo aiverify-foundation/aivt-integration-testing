68e84dfcf8f07261355b5e4a44a76c45a00c4eed
Benedict Lee <benedict_lee@imda.gov.sg>
2023-05-12 20:45:47 +0800

    fix(dataset-data.js): Update Dataset GraphQL Queries

--- tests/graphql/dataset/dataset-data.js
+++ tests/graphql/dataset/dataset-data.js
@@ -1,29 +1,54 @@
-export const DATASETS = `query Datasets {
-    datasets {
+export const DATASETS = `query Query {
+  datasets {
+    id
+    filename
+    name
+    filePath
+    type
+    ctime
+    dataColumns {
       id
-      filename
       name
-      filePath
-      subFolder
-      type
-      ctime
-      dataColumns {
-        id
-        name
-        datatype
-        label
-        sensitive
-      }
-      numRows
-      numCols
-      description
-      status
-      size
-      serializer
-      dataFormat
-      errorMessages
+      datatype
+      label
+      sensitive
     }
-  }`
+    numRows
+    numCols
+    description
+    status
+    size
+    serializer
+    dataFormat
+    errorMessages
+  }
+}`
+
+export const UPDATE_DATASET = `mutation Mutation($datasetId: ObjectID!, $dataset: DatasetInput) {
+  updateDataset(datasetID: $datasetId, dataset: $dataset) {
+    id
+    filename
+    name
+    filePath
+    type
+    ctime
+    dataColumns {
+      id
+      name
+      datatype
+      label
+      sensitive
+    }
+    numRows
+    numCols
+    description
+    status
+    size
+    serializer
+    dataFormat
+    errorMessages
+  }
+}`
 
 export const DELETE_DATASET = `mutation Mutation($deleteDatasetId: ObjectID!) {
     deleteDataset(id: $deleteDatasetId)
