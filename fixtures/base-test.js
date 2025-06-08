3a3f80da6da8099d04be1593bf9675776d9ea80c
Benedict Lee <118954493+imda-benedictlee@users.noreply.github.com>
2025-06-08 21:41:21 +0800

    Clean Up Code and Add Create Project Page, Select Data Page, Report Template Page, Canvas Page and User Input Page to Base Test

--- fixtures/base-test.js
+++ fixtures/base-test.js
@@ -1,19 +1,32 @@
 const base = require('@playwright/test');
-import { HomePage } from './home-page'
+import { HomePage } from './home-page';
+import { CreateProjectPage } from './create-project-page';
 import { ManagePage } from './manage-page';
+import { SelectDataPage } from './select-data-page';
+import { CanvasPage } from './canvas-page';
 import { ModelPage } from './model-page';
 import { DatasetPage } from './dataset-page';
-import { TestResultPage } from './test-result-page';
+import { ReportTemplatePage } from './report-template-page';
 import { UserInputPage } from './user-input-page';
+import { TestResultPage } from './test-result-page';
 
-// Extend base test by providing "homePage","managePage","modelPage","datasetPage","testResultPage","userInputPage" functions and elements to test
+// Extend base test by providing "homePage","createProjectPage","managePage","selectDataPage","canvasPage","modelPage","datasetPage","reportTemplatePage","userInputPage","testResultPage" functions and elements to test
 // This new "test" can be used in multiple test files, and each of them will get the fixtures.
 exports.test = base.test.extend({
     homePage: async ({ page }, use) => {
       await use(new HomePage(page));
     },
+    createProjectPage: async ({ page }, use) => {
+      await use(new CreateProjectPage(page));
+    },
     managePage: async ({ page }, use) => {
       await use(new ManagePage(page));
+    },
+    canvasPage: async ({ page }, use) => {
+      await use (new CanvasPage(page));
+    },
+    selectDataPage: async ({ page }, use) => {
+      await use(new SelectDataPage(page));
     },
     modelPage: async ({ page }, use) => {
       await use(new ModelPage(page));
@@ -21,11 +34,14 @@ exports.test = base.test.extend({
     datasetPage: async ({ page }, use) => {
       await use(new DatasetPage(page));
     },
-    testResultPage: async ({ page }, use) => {
-      await use(new TestResultPage(page));
+    reportTemplatePage: async ({ page }, use) => {
+      await use(new ReportTemplatePage(page));
     },
     userInputPage: async ({ page }, use) => {
       await use(new UserInputPage(page));
+    },
+    testResultPage: async ({ page }, use) => {
+      await use(new TestResultPage(page));
     }
   });
   
