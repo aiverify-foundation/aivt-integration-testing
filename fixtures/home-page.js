debaf6db3b5068db10bfb3b2a3570d996ebce78a
Benedict Lee <118954493+imda-benedictlee@users.noreply.github.com>
2025-06-08 21:42:59 +0800

    Add New Elements and Function to Home Page Fixtures

--- fixtures/home-page.js
+++ fixtures/home-page.js
@@ -1,3 +1,5 @@
+import { expect } from './base-test'
+
 export class HomePage {
   /**
    * @param {import('@playwright/test').Page} page
@@ -8,14 +10,27 @@ export class HomePage {
     /* Home Page */
     this.aivlogo = page.getByRole('link', { name: 'AI Verify' });
     this.manageButton = page.getByRole('link', { name: 'Manage Models, Datasets, etc' });
+    this.createProjectButton = page.getByRole('link', { name: 'Test an AI Model and generate' });
     
   }
 
   /**
-   * @param {string}
+   * @param { string }
    */
   async goto(url) {
     await this.page.goto(url)
+  }
+
+  /**
+   * @param { string }
+   */
+  async validateProjectCreation(reportTitle) {
+
+    /* Validate Project Creation */
+    console.log('[INFO] Validate Project Creation')
+    await expect(this.page.getByRole('heading', { name: reportTitle, exact: true })).toBeVisible()
+    console.log('[INFO] Project Created')
+
   }
 
 }
