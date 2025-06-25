97a0026631b8ace2dd537350f86013fa061edd5d
Benedict Lee <118954493+imda-benedictlee@users.noreply.github.com>
2025-06-25 21:29:29 +0800

    Change Trace Option to On

--- playwright.config.js
+++ playwright.config.js
@@ -42,7 +42,7 @@ const config = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
     video: 'on',
     launchOptions: {
       slowMo: 500
