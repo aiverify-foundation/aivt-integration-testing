2a809c87b50d49b5112804fdac2fd45e79640602
Benedict Lee <118954493+imda-benedictlee@users.noreply.github.com>
2025-06-28 12:05:58 +0800

    Reorder Base Test Fixtures

--- fixtures/base-test.js
+++ fixtures/base-test.js
@@ -1,47 +1,51 @@
 const base = require('@playwright/test');
-import { HomePage } from './home-page';
-import { CreateProjectPage } from './create-project-page';
-import { ManagePage } from './manage-page';
-import { SelectDataPage } from './select-data-page';
 import { CanvasPage } from './canvas-page';
-import { ModelPage } from './model-page';
+import { CreateProjectPage } from './create-project-page';
 import { DatasetPage } from './dataset-page';
+import { HomePage } from './home-page';
+import { ManagePage } from './manage-page';
+import { ModelPage } from './model-page';
+import { PluginPage } from './plugin';
 import { ReportTemplatePage } from './report-template-page';
-import { UserInputPage } from './user-input-page';
+import { SelectDataPage } from './select-data-page';
 import { TestResultPage } from './test-result-page';
+import { UserInputPage } from './user-input-page';
 
-// Extend base test by providing "homePage","createProjectPage","managePage","selectDataPage","canvasPage","modelPage","datasetPage","reportTemplatePage","userInputPage","testResultPage" functions and elements to test
+// Extend base test by providing "canvasPage","createProjectPage","datasetPage","homePage","managePage","modelPage","pluginPage","reportTemplatePage","selectDataPage","testResultPage","userInputPage" functions and elements to test
 // This new "test" can be used in multiple test files, and each of them will get the fixtures.
 exports.test = base.test.extend({
-    homePage: async ({ page }, use) => {
-      await use(new HomePage(page));
+    canvasPage: async ({ page }, use) => {
+      await use(new CanvasPage(page));
     },
     createProjectPage: async ({ page }, use) => {
       await use(new CreateProjectPage(page));
     },
+    datasetPage: async ({ page }, use) => {
+      await use(new DatasetPage(page));
+    },
+    homePage: async ({ page }, use) => {
+      await use (new HomePage(page));
+    },
     managePage: async ({ page }, use) => {
       await use(new ManagePage(page));
-    },
-    canvasPage: async ({ page }, use) => {
-      await use (new CanvasPage(page));
-    },
-    selectDataPage: async ({ page }, use) => {
-      await use(new SelectDataPage(page));
     },
     modelPage: async ({ page }, use) => {
       await use(new ModelPage(page));
     },
-    datasetPage: async ({ page }, use) => {
-      await use(new DatasetPage(page));
+    pluginPage: async ({ page }, use) => {
+      await use(new PluginPage(page));
     },
     reportTemplatePage: async ({ page }, use) => {
       await use(new ReportTemplatePage(page));
     },
-    userInputPage: async ({ page }, use) => {
-      await use(new UserInputPage(page));
+    selectDataPage: async ({ page }, use) => {
+      await use(new SelectDataPage(page));
     },
     testResultPage: async ({ page }, use) => {
       await use(new TestResultPage(page));
+    },
+    userInputPage: async ({ page }, use) => {
+      await use(new UserInputPage(page));
     }
   });
   
