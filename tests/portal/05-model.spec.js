3f0955359dec825b2d1ad00f404ab5c4f333452e
Benedict Lee <118954493+imda-benedictlee@users.noreply.github.com>
2025-07-02 13:41:43 +0800

    Add Remaining Test Cases For Model Page

--- tests/portal/05-model.spec.js
+++ tests/portal/05-model.spec.js
@@ -209,7 +209,7 @@ test.describe('View Uploaded Models', () => {
         await page.getByRole("heading", { name: editModelData.modelName, exact: true }).click()
 
         await expect.soft(page.getByRole("heading", { name: editModelData.modelName, exact: true })).toBeVisible()
-        await expect.soft(page.getByText('Model Type: ' + editModelData.modelType )).toBeVisible()
+        await expect.soft(page.getByText('Model Type: ' + editModelData.modelType)).toBeVisible()
 
         console.log('[INFO] Revert Details')
         await modelPage.editModelDetailsButton.click()
@@ -323,4 +323,367 @@ test.describe('View Uploaded Models', () => {
         await expect.soft(modelPage.page2Button).not.toBeVisible()
 
     })
+})
+
+test.describe('Upload Model', () => {
+
+    test.beforeEach(async ({ homePage, managePage, modelPage }) => {
+
+        /* AI Verify Homepage */
+        console.log('[INFO] Navigate to AI Verify Home Page')
+        await homePage.goto(url + ":" + port_number)
+        await expect.soft(homePage.aivlogo).toBeVisible({ timeout: 60000 })
+        await homePage.manageButton.click()
+
+        /* Manage Page */
+        console.log('[INFO] Manage Page')
+        await managePage.modelButton.click()
+
+        /* Upload AI Model */
+        console.log('[INFO] Upload AI Models')
+        await modelPage.uploadModelButton.click()
+
+    })
+
+    test('Upload AI Model Option', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+
+        /* Assert Upload AI Model Option */
+        await expect(page.getByRole('heading', { name: 'Add New AI Model > Upload' })).toBeVisible()
+
+    })
+
+    test('Upload Pipeline Option', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload Pipeline')
+        await modelPage.uploadAIModelPipelineButton.click()
+        await modelPage.nextButton.click()
+
+        /* Assert Upload Pipeline Option */
+        await expect(page.getByRole('heading', { name: 'Add New AI Model > Upload Model Pipeline' })).toBeVisible()
+
+    })
+
+    test('Upload Valid Model File - Drag and Drop', async ({ modelPage }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+
+        let filePathStringArray = [root_path + '/model/' + modelName]
+        await modelPage.dragAndDropFile(filePathStringArray)
+
+    })
+
+    test('Upload Valid Model File - Click To Browse', async ({ modelPage }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+
+        let filePathStringArray = [root_path + '/model/' + modelName]
+        await modelPage.uploadFile(filePathStringArray)
+
+    })
+
+    test('Upload Valid Model Than One Model File', async ({ modelPage }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+
+        let filePathStringArray = [root_path + '/model/' + modelName, root_path + '/model/sample_mc_toxic_sklearn_linear.LogisticRegression.sav']
+        await modelPage.uploadFile(filePathStringArray)
+
+    })
+
+    test('Upload More Than 10 Model Files', async ({ modelPage }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+
+        // let filePathStringArray = [
+        //     root_path + '/model/' + "sample_bc_credit_sklearn_linear.LogisticRegression_0.sav", root_path + '/model/sample_mc_toxic_sklearn_linear.LogisticRegression.sav', root_path + '/model/sample_reg_donation_sklearn_linear.LinearRegression.sav',
+        //     root_path + '/model/' + modelName, root_path + '/model/sample_mc_toxic_sklearn_linear.LogisticRegression.sav', root_path + '/model/sample_reg_donation_sklearn_linear.LinearRegression.sav',
+        //     root_path + '/model/' + modelName, root_path + '/model/sample_mc_toxic_sklearn_linear.LogisticRegression.sav', root_path + '/model/sample_reg_donation_sklearn_linear.LinearRegression.sav',
+        //     root_path + '/model/' + modelName, root_path + '/model/sample_mc_toxic_sklearn_linear.LogisticRegression.sav', root_path + '/model/sample_reg_donation_sklearn_linear.LinearRegression.sav',
+        // ]
+
+        let filePathStringArray = [
+            root_path + '/model/sample_bc_credit_sklearn_linear.LogisticRegression_0.sav',
+            root_path + '/model/sample_bc_credit_sklearn_linear.LogisticRegression_1.sav',
+            root_path + '/model/sample_bc_credit_sklearn_linear.LogisticRegression_2.sav',
+            root_path + '/model/sample_bc_credit_sklearn_linear.LogisticRegression_3.sav',
+            root_path + '/model/sample_bc_credit_sklearn_linear.LogisticRegression_4.sav',
+            root_path + '/model/sample_bc_credit_sklearn_linear.LogisticRegression_5.sav',
+            root_path + '/model/sample_bc_credit_sklearn_linear.LogisticRegression_6.sav',
+            root_path + '/model/sample_bc_credit_sklearn_linear.LogisticRegression_7.sav',
+            root_path + '/model/sample_bc_credit_sklearn_linear.LogisticRegression_8.sav',
+            root_path + '/model/sample_bc_credit_sklearn_linear.LogisticRegression_9.sav',
+            root_path + '/model/sample_bc_credit_sklearn_linear.LogisticRegression_10.sav'
+        ]
+        await modelPage.uploadFile(filePathStringArray)
+
+    })
+
+    test('Upload Invalid Model File', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+
+        await page.locator('#fileInput').setInputFiles(root_path + '/data/sample_bc_credit_data.sav')
+
+        await modelPage.modelTypeComboBox.click()
+        await modelPage.modelTypeComboBox.selectOption('classification')
+
+        await modelPage.uploadFileButton.click()
+
+        /* Assert Uploading Invalid Files */
+        await expect(page.getByText('Error uploading files.')).toBeVisible()
+
+    })
+
+    test('Model Type Is Not Selected', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+
+        await page.locator('#fileInput').setInputFiles(root_path + '/model/' + modelName)
+
+        await modelPage.uploadFileButton.click()
+
+        /* Assert Model Type Is Not Selected */
+        await expect.soft(page.getByText("Files uploaded successfully!")).not.toBeVisible()
+
+    })
+
+    test('Remove Model File To Be Uploaded', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+
+        await page.locator('#fileInput').setInputFiles(root_path + '/model/' + modelName)
+        await modelPage.removeModelFile.nth(2).click()
+
+        /* Assert Remove Model File To Be Uploaded */
+        await expect.soft(page.getByText(modelName)).not.toBeVisible()
+
+    })
+
+    test('Upload Valid Model Folder - Click To Browse', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+
+        let folderPathStringArray = [root_path + "/model/tensorflow_tabular_sequential.sav"]
+        await modelPage.uploadFolder(folderPathStringArray, "folderInput")
+
+    })
+
+    test('Empty Model Folder Name', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+        await modelPage.folderButton.click()
+        await modelPage.page.locator('#folderInput').setInputFiles(root_path + "/model/tensorflow_tabular_sequential.sav")
+        await modelPage.modelFolderNameTextBox.clear()
+        await modelPage.modelTypeComboBox.click()
+        await modelPage.modelTypeComboBox.selectOption('classification')
+        await modelPage.uploadFolderButton.click()
+
+        /* Assert Empty Model Folder Name */
+        await expect.soft(page.getByText('Folder uploaded successfully!')).not.toBeVisible()
+
+    })
+
+    test('Upload More Than One Model Folder', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+        await modelPage.folderButton.click()
+
+        await modelPage.page.locator('#folderInput').setInputFiles(root_path + "/model/tensorflow_tabular_sequential.sav")
+        await modelPage.page.locator('#folderInput').setInputFiles(root_path + "/model/tensorflow_tabular_sequential2.sav")
+
+        /* Assert Upload More Than One Model Folder */
+        await expect.soft(page.getByRole('heading', { name: 'tensorflow_tabular_sequential2.sav/ (2 files)'})).toBeVisible()
+        await expect.soft(page.getByRole('heading', { name: 'tensorflow_tabular_sequential2.sav/variables/ (2 files)' })).toBeVisible()
+        await expect.soft(page.getByRole('heading', { name: 'tensorflow_tabular_sequential.sav/ (2 files)'})).not.toBeVisible()
+        await expect.soft(page.getByRole('heading', { name: 'tensorflow_tabular_sequential.sav/variables/ (2 files)' })).not.toBeVisible()
+
+    })
+
+    test('Upload Invalid Model Folder', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+        await modelPage.folderButton.click()
+        await modelPage.page.locator('#folderInput').setInputFiles(root_path + "/data/raw_fashion_image_10")
+        await modelPage.modelTypeComboBox.click()
+        await modelPage.modelTypeComboBox.selectOption('classification')
+        await modelPage.uploadFolderButton.click()
+
+        /* Assert Upload Invalid Model Folder */
+        expect.soft(await page.getByText('Error uploading folder:')).toBeVisible()
+
+    })
+
+    test('Model Type Is Not Selected Model Folder', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+        await modelPage.folderButton.click()
+        await modelPage.page.locator('#folderInput').setInputFiles(root_path + "/model/tensorflow_tabular_sequential.sav")
+        await modelPage.uploadFolderButton.click()
+
+        /* Assert Upload Invalid Model Folder */
+        await expect.soft(page.getByText("Files uploaded successfully!")).not.toBeVisible()
+
+    })
+
+    test('Clear All Button', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+        await modelPage.folderButton.click()
+        await page.locator('#folderInput').setInputFiles(root_path + "/model/tensorflow_tabular_sequential.sav")
+        await modelPage.clearAllButton.click()
+
+        /* Assert Upload Invalid Model Folder */
+        await expect.soft(page.getByText("tensorflow_tabular_sequential.sav")).not.toBeVisible()
+
+    })
+
+    test('Cancel Button Model Folder', async ({ modelPage }) => {
+
+        console.log('[INFO] Upload AI Model')
+        await modelPage.uploadAIModelButton.click()
+        await modelPage.nextButton.click()
+        await modelPage.folderButton.click()
+        await modelPage.cancelButton.click()
+
+        /* Assert Cancel Button */
+        await expect.soft(modelPage.uploadAIModelButton).toBeVisible()
+        await expect.soft(modelPage.uploadAIModelPipelineButton).toBeVisible()
+        
+    })
+
+    test('Upload Valid Pipeline Folder - Click To Browse', async ({ modelPage }) => {
+
+        let folderPathStringArray = [root_path + '/pipeline/bc_image_face']
+
+        console.log('[INFO] Upload AI Model Pipelines')
+        await modelPage.uploadAIModelPipelineButton.click()
+        await modelPage.nextButton.click()
+
+        /* Assert Upload Valid Pipeline Folder */
+        await modelPage.uploadFolder(folderPathStringArray, "pipelineInput")
+        
+    })
+
+    test('Upload More Than One Pipeline Folder', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model Pipelines')
+        await modelPage.uploadAIModelPipelineButton.click()
+        await modelPage.nextButton.click()
+
+        await page.locator('#pipelineInput').setInputFiles(root_path + '/pipeline/bc_image_face')
+        await page.locator('#pipelineInput').setInputFiles(root_path + '/pipeline/bc_tabular_credit')
+        await modelPage.uploadFolderCloseDialogButton.click()
+
+        /* Assert Upload More Than One Model Folder */
+        await expect.soft(page.getByRole('heading', { name: 'bc_tabular_credit'})).toBeVisible()
+        await expect.soft(page.getByRole('heading', { name: 'bc_image_face'})).toBeVisible()
+
+    })
+
+    test('Upload Invalid Pipeline Folder', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model Pipelines')
+        await modelPage.uploadAIModelPipelineButton.click()
+        await modelPage.nextButton.click()
+
+        await page.locator('#pipelineInput').setInputFiles(root_path + '/data/raw_fashion_image_10')
+        await modelPage.uploadFolderCloseDialogButton.click()
+        await modelPage.modelTypeComboBox.first().click()
+        await modelPage.modelTypeComboBox.first().selectOption('classification')
+        await modelPage.uploadPipelineButton.click()
+
+        /* Assert Upload Invalid Pipeline Folder */
+        await expect.soft(page.getByText('Upload completed: 0')).toBeVisible()
+
+    })
+
+    test('Pipeline Model Type Not Selected', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model Pipelines')
+        await modelPage.uploadAIModelPipelineButton.click()
+        await modelPage.nextButton.click()
+
+        await page.locator('#pipelineInput').setInputFiles(root_path + '/data/raw_fashion_image_10')
+        await modelPage.uploadFolderCloseDialogButton.click()
+        await modelPage.modelTypeComboBox.first().click()
+        await modelPage.uploadPipelineButton.click()
+
+        /* Assert Upload Invalid Pipeline Folder */
+        await expect.soft(page.getByText('Upload completed: 1 successful, 0 failed.')).not.toBeVisible()
+
+    })
+
+    test('Remove Button', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model Pipelines')
+        await modelPage.uploadAIModelPipelineButton.click()
+        await modelPage.nextButton.click()
+
+        await page.locator('#pipelineInput').setInputFiles(root_path + '/pipeline/bc_image_face')
+        await modelPage.uploadFolderCloseDialogButton.click()
+        await modelPage.removeButton.click()
+
+        /* Assert Remove Button */
+        await expect.soft(page.getByRole('heading', { name: 'bc_image_face(2 files)' })).not.toBeVisible()
+
+    })
+
+    test('Clear All Button Pipeline Folder', async ({ modelPage, page }) => {
+
+        console.log('[INFO] Upload AI Model Pipelines')
+        await modelPage.uploadAIModelPipelineButton.click()
+        await modelPage.nextButton.click()
+
+        await page.locator('#pipelineInput').setInputFiles(root_path + '/pipeline/bc_image_face')
+        await modelPage.uploadFolderCloseDialogButton.click()
+        await modelPage.clearAllButton.click()
+
+        /* Assert Clear All Button */
+        await expect.soft(page.getByRole('heading', { name: 'bc_image_face(2 files)' })).not.toBeVisible()
+
+    })
+
+    test('Cancel Button Pipeline Folder', async ({ modelPage }) => {
+
+        console.log('[INFO] Upload AI Model Pipelines')
+        await modelPage.uploadAIModelPipelineButton.click()
+        await modelPage.nextButton.click()
+        await modelPage.cancelButton.click()
+
+        /* Assert Cancel Button */
+        await expect.soft(modelPage.uploadAIModelButton).toBeVisible()
+        await expect.soft(modelPage.uploadAIModelPipelineButton).toBeVisible()
+
+    })
+
 })
