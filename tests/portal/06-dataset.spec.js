cd4eb854c345da6b65e28fba1831f8bc0d47601c
Benedict Lee <118954493+imda-benedictlee@users.noreply.github.com>
2025-07-02 22:36:19 +0800

    Add Remaining Test Cases for Dataset

--- tests/portal/06-dataset.spec.js
+++ tests/portal/06-dataset.spec.js
@@ -1,5 +1,4 @@
 import { test, expect } from '../../fixtures/base-test'
-import { setTimeout } from "timers/promises"
 
 const url = process.env.URL
 const port_number = process.env.PORT_NUMBER
@@ -77,9 +76,166 @@ test.describe('View Uploaded Dataset', () => {
         await datasetPage.deleteDialogBoxButton.click()
 
         /* Assert Delete Dataset Button */
-        await expect(page.getByText('Datasets deleted successfully!')).toBeVisible()
-        await expect(datasetPage.algorithmNameCell.nth(1).textContent()).not.toBe(algorithmName)
+        await expect.soft(page.getByText('Datasets deleted successfully!')).toBeVisible()
+        await expect.soft(datasetPage.algorithmNameCell.nth(1).textContent()).not.toBe(algorithmName)
+
+    })
+
+    test('Upload Dataset File - Drag And Drop', async ({ datasetPage }) => {
+
+        /* Upload Dataset File */
+        console.log('[INFO] Upload Dataset Files')
+        let filePathStringArray = [root_path + '/data/pickle_pandas_fashion_mnist_annotated_labels_10.sav']
+        await datasetPage.uploadDatasetButton.click()
+        await datasetPage.dragAndDropFile(filePathStringArray)
+
+    })
+
+    test('Upload Dataset File - Click To Browse', async ({ datasetPage }) => {
+
+        /* Upload Dataset File */
+        console.log('[INFO] Upload Dataset Files')
+        let filePathStringArray = [root_path + '/data/pickle_pandas_fashion_mnist_annotated_labels_10.sav']
+        await datasetPage.uploadDatasetButton.click()
+        await datasetPage.uploadFile(filePathStringArray)
+
+    })
+
+    test('Upload More Than One Dataset', async ({ datasetPage }) => {
+
+        /* Upload Dataset File */
+        console.log('[INFO] Upload Dataset Files')
+        let filePathStringArray = [root_path + '/data/pickle_pandas_fashion_mnist_annotated_labels_10.sav', root_path + '/data/sample_bc_pipeline_credit_data.sav']
+        await datasetPage.uploadDatasetButton.click()
+        await datasetPage.uploadFile(filePathStringArray)
+
+    })
+
+    test('Upload More Than 10 Dataset', async ({ datasetPage }) => {
+
+        /* Upload Dataset File */
+        console.log('[INFO] Upload Dataset Files')
+        filePathStringArray = [
+            root_path + '/data/pickle_pandas_fashion_mnist_annotated_labels_10.sav', root_path + '/data/sample_bc_credit_data.sav', root_path + '/data/sample_bc_pipeline_credit_data.sav',
+            root_path + '/data/sample_bc_pipeline_credit_ytest_data.sav', root_path + '/data/sample_mc_pipeline_toxic_data.sav', root_path + '/data/sample_mc_pipeline_toxic_ytest_data.sav',
+            root_path + '/data/sample_mc_toxic_data.sav', root_path + '/data/sample_reg_donation_data.sav', root_path + '/data/sample_reg_pipeline_data.sav',
+            root_path + '/data/sample_reg_pipeline_ytest_data.sav', root_path + '/data/true']
+        await datasetPage.uploadDatasetButton.click()
+        await datasetPage.uploadFile(filePathStringArray)
+
+    })
+
+    test('Upload Invalid Dataset File', async ({ datasetPage, page }) => {
+
+        /* Upload Dataset File */
+        console.log('[INFO] Upload Dataset Files')
+        await datasetPage.uploadDatasetButton.click()
+        await page.locator('#fileInput').setInputFiles(root_path + '/model/sample_bc_credit_sklearn_linear.LogisticRegression.sav')
+        await datasetPage.uploadFileButton.click()
+
+        /* Assert Upload Invalid Dataset File */
+        await expect.soft(page.getByText('Some files failed to upload.')).toBeVisible()
+        await expect.soft(page.getByText('Unsupported Dataset')).toBeVisible()
+
+    })
+
+    test('Remove Dataset File To Be Uploaded', async ({ datasetPage, page }) => {
+
+        /* Upload Dataset File */
+        console.log('[INFO] Upload Dataset Files')
+        await datasetPage.uploadDatasetButton.click()
+        await page.locator('#fileInput').setInputFiles(root_path + '/data/pickle_pandas_fashion_mnist_annotated_labels_10.sav')
+        await datasetPage.removeDatasetFile.click()
+
+        /* Assert Remove Dataset File To Be Uploaded */
+        await expect.soft(page.getByText('pickle_pandas_fashion_mnist_annotated_labels_10.sav')).not.toBeVisible()
+
+    })
+
+    test('Upload Valid Dataset Folder - Click To Browse', async ({ datasetPage }) => {
+
+        /* Upload Dataset Folder */
+        let folderPathStringArray = [root_path + '/data/raw_fashion_image_10', root_path + '/data/small_test']
+
+        console.log('[INFO] Upload Dataset Folder')
+        await datasetPage.uploadDatasetButton.click()
+        await datasetPage.uploadDatasetFolderButton.click()
+        await datasetPage.uploadFolder(folderPathStringArray)
+
+    })
+
+    test('Upload More Than One Dataset Folder', async ({ datasetPage, page }) => {
+
+        /* Upload Dataset Folder */
+        console.log('[INFO] Upload Dataset Folder')
+        await datasetPage.uploadDatasetButton.click()
+        await datasetPage.uploadDatasetFolderButton.click()
+        await page.locator('#folderInput').setInputFiles(root_path + '/data/raw_fashion_image_10')
+        await page.locator('#folderInput').setInputFiles(root_path + '/data/small_test')
+        await datasetPage.uploadFolderCloseDialogButton.click()
+
+        /* Assert Upload More Than One Dataset Folder */
+        await expect.soft(page.getByRole('heading', { name: 'raw_fashion_image_10' })).not.toBeVisible()
+        await expect.soft(page.getByRole('heading', { name: 'small_test' })).toBeVisible()
+
+    })
+
+    test('Upload Invalid Dataset Folder', async ({ datasetPage, page }) => {
+
+        /* Upload Dataset Folder */
+        console.log('[INFO] Upload Dataset Folder')
+        await datasetPage.uploadDatasetButton.click()
+        await datasetPage.uploadDatasetFolderButton.click()
+        await page.locator('#folderInput').setInputFiles(root_path + '/model/tensorflow_tabular_sequential.sav')
+        await datasetPage.uploadFolderCloseDialogButton.click()
+        await datasetPage.uploadFolderButton.click()
+
+        /* Assert Upload Invalid Dataset Folder */
+        await expect.soft(page.getByText('Upload completed: 0')).toBeVisible()
+
+    })
+
+    test('Clear All Button', async ({ datasetPage, page }) => {
+
+        /* Upload Dataset Folder */
+        console.log('[INFO] Upload Dataset Folder')
+        await datasetPage.uploadDatasetButton.click()
+        await datasetPage.uploadDatasetFolderButton.click()
+        await page.locator('#folderInput').setInputFiles(root_path + '/data/raw_fashion_image_10')
+        await datasetPage.uploadFolderCloseDialogButton.click()
+        await datasetPage.clearAllButton.click()
+
+        /* Assert Clear All Button */
+        await expect.soft(page.getByRole('headings', { name: 'raw_fashion_image_10'})).not.toBeVisible()
+
+    })
+
+    test('Remove Dataset Folder Button', async ({ datasetPage, page }) => {
+
+        /* Upload Dataset Folder */
+        console.log('[INFO] Upload Dataset Folder')
+        await datasetPage.uploadDatasetButton.click()
+        await datasetPage.uploadDatasetFolderButton.click()
+        await page.locator('#folderInput').setInputFiles(root_path + '/data/raw_fashion_image_10')
+        await datasetPage.uploadFolderCloseDialogButton.click()
+        await datasetPage.removeDatasetFolder.click()
         
+        /* Assert Remove Dataset Folder Button */
+        await expect.soft(page.getByRole('headings', { name: 'raw_fashion_image_10'})).not.toBeVisible()
+
+    })
+
+    test('View Dataset Button', async ({ datasetPage, page }) => {
+
+        /* Upload Dataset Folder */
+        console.log('[INFO] Upload Dataset Folder')
+        await datasetPage.uploadDatasetButton.click()
+        await datasetPage.uploadDatasetFolderButton.click()
+        await datasetPage.viewDatasetButton.click()
+        
+        /* Assert View Dataset Button */
+        await expect.soft(page.getByRole('heading', { name: 'Test Datasets', exact: true })).toBeVisible()
+
     })
 
 })
