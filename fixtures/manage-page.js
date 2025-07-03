3e7b53396397acf4ce0a882957267dacbb1173ad
Benedict Lee <118954493+imda-benedictlee@users.noreply.github.com>
2025-07-03 17:17:19 +0800

    Add Report Template Link Fixture in Manage Page

--- fixtures/manage-page.js
+++ fixtures/manage-page.js
@@ -8,6 +8,7 @@ export class ManagePage {
         /* Manage Page */
         this.modelButton = page.getByRole('link', { name: 'Manage Models Models' });
         this.datasetButton = page.getByRole('link', { name: 'Manage datasets Data' });
+        this.reportTemplateButton = page.getByRole('link', { name: 'Manage report templates' });
         this.testResultButton = page.getByRole('link', { name: 'Manage test results Test' });
         this.userInputButton = page.getByRole('link', { name: 'Manage user inputs User Inputs' });
     }
