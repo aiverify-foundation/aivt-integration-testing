e84259e5c55a90bd901568138e7f338ac3e9a76e
Benedict Lee <118954493+imda-benedictlee@users.noreply.github.com>
2025-07-03 18:16:02 +0800

    Add Additional Fixtures

--- fixtures/report-template-page.js
+++ fixtures/report-template-page.js
@@ -1,3 +1,5 @@
+import { readFileSync } from 'fs'
+
 export class ReportTemplatePage {
     /**
      * @param {import('@playwright/test').Page} page
@@ -5,11 +7,20 @@ export class ReportTemplatePage {
     constructor(page) {
         this.page = page;
 
-        /* Search Bar */
+        /* Report Template Page */
         this.reportTemplateSearchBar = page.getByPlaceholder('Search templates...')
-
-        /* Report Template */
+        this.uploadTemplateButton = page.getByRole('button', { name: 'UPLOAD TEMPLATE' })
+        this.clearSearchButton = page.getByRole('button', { name: 'Clear' })
         this.editReportTemplateButton = page.locator('.transition-colors')
+        this.viewReportTemplateButton = page.locator('.transition-colors').first()
+        this.copyReportTemplateButton = page.locator('.undefined > button:nth-child(2)').first()
+
+        /* Edit Mode */
+        this.editModeButton = page.getByRole('button', { name: 'Switch to Edit Mode' })
+        this.viewModeButton = page.getByRole('button', { name: 'Switch to View Mode' })
+
+        /* Upload Report Template */
+        this.removeReportTemplateFileButton = page.getByRole('button').nth(1)
 
     }
 
@@ -20,8 +31,8 @@ export class ReportTemplatePage {
 
         /* Report Template */
         console.log('[INFO] Select Report Template');
-        await this.page.getByRole('heading',{ name: reportTemplateName, exact: true }).click();
-        
+        await this.page.getByRole('heading', { name: reportTemplateName, exact: true }).click();
+
     }
 
     /**
@@ -31,5 +42,22 @@ export class ReportTemplatePage {
 
         await this.reportTemplateSearchBar.fill(reportTemplateName);
 
+    }
+
+    /**
+   * @param { string array }
+   */
+    async dragAndDropFile(filePathStringArray) {
+        for (const filePath of filePathStringArray) {
+            const bufferData = readFileSync(filePath).toString('base64');
+            const dataTransfer = await this.page.evaluateHandle(async (data) => {
+                const transferData = new DataTransfer();
+                const blobData = await fetch(data).then(res => res.blob());
+                const file = new File([blobData], 'Project-1-12.data.json', { type: 'application/json' });
+                transferData.items.add(file);
+                return transferData;
+            }, 'data:application/octet-stream;base64,' + bufferData);
+            await this.page.dispatchEvent('#fileInput', 'drop', { dataTransfer });
+        }
     }
 }
