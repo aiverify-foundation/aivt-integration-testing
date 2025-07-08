0acd2929e44f749816cbf2e1b1fe68a6ff13d7fc
Benedict Lee <118954493+imda-benedictlee@users.noreply.github.com>
2025-07-08 20:10:23 +0800

    Rearrange Fixtures

--- fixtures/manage-page.js
+++ fixtures/manage-page.js
@@ -9,9 +9,9 @@ export class ManagePage {
         this.modelButton = page.getByRole('link', { name: 'Manage Models Models' });
         this.datasetButton = page.getByRole('link', { name: 'Manage datasets Data' });
         this.reportTemplateButton = page.getByRole('link', { name: 'Manage report templates' });
-        this.testResultButton = page.getByRole('link', { name: 'Manage test results Test' });
-        this.pluginButton = page.getByRole('link', { name: 'Manage plugins Plugins' });
         this.userInputButton = page.getByRole('link', { name: 'Manage user inputs User Inputs' });
+        this.pluginButton = page.getByRole('link', { name: 'Manage plugins Plugins' });
+        this.testResultButton = page.getByRole('link', { name: 'Manage test results Test' });
     }
 
 }
