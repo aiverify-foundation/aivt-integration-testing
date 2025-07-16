c4a54a81510d043301475d4d7409583d2e509b04
Benedict Lee <118954493+imda-benedictlee@users.noreply.github.com>
2025-07-16 18:56:54 +0800

    Change Playwright Config

--- playwright.config.js
+++ playwright.config.js
@@ -42,8 +42,8 @@ const config = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
-    video: 'on',
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure',
     launchOptions: {
       slowMo: 500
     },
